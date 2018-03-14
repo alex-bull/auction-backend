456522ebb01ced6a155346d7c1494c741f4ac2f9
Alex Bull <abu59@uclive.ac.nz>
2018-03-15 01:50:40 +1300

    Refactored to involve pooling

--- app.js
+++ app.js
@@ -6,15 +6,13 @@ app.use(bodyParser.json());
 
 const mysql = require('mysql');
 
-function connect(){
-    let con = mysql.createConnection({
-        host: 'mysql3.csse.canterbury.ac.nz',
-        user: 'abu59',
-        password: '11213398',
-        database: 'abu59'
-    });
-    return con;
-}
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'mysql3.csse.canterbury.ac.nz',
+    user: 'abu59',
+    password: '11213398',
+    database: 'abu59'
+});
 
 // const con = connect();
 // con.connect(function(err){
@@ -25,25 +23,29 @@ function connect(){
 //     });
 // });
 
-app.get("/auction", function (req, res){
-    const con = connect();
-    con.connect(function (err){
-        if (!err){
-            console.log("Connected to the database");
-            con.query('SELECT * from auction ORDER BY auction_startingdate', function(err, rows, fields){
-                con.end();
-                if(!err){
-                    res.send(JSON.stringify(rows));
-                } else {
-                    console.log(err);
-                    res.send({"ERROR": "Error getting users"});
-                }
-            });
-        } else {
-            console.log("Error connecting to database");
-            res.send({"ERROR": "Error connecting to database"});
+function get_auctions(req, res) {
+    pool.getConnection(function(err, connection){
+        if(err){
+            console.log(err);
+            res.json({"ERROR": "Error in connection database"});
+            return;
         }
+        console.log('connected as id ' + connection.threadId);
+        connection.query("SELECT * from auction ORDER BY auction_startingdate", function(err, rows){
+            connection.release();
+            if(!err){
+                res.json(rows);
+            }
+        });
+        connection.on('error', function(err){
+            res.json({"ERROR": "Error in connection database"});
+            return;
+        });
     });
+}
+
+app.get("/auction", function(req, res){
+    get_auctions(req, res);
 });
 
 
